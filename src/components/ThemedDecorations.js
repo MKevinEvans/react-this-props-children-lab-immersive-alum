d6f38100eb58498b6ff888a26413bdf1fd867178
Kevin Evans <mkevinevans@gmail.com>
2019-08-02 19:19:09 +0000

    Automatically backed up by Learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,17 @@
 // Code ThemedDecoration Component Here
+import React from 'react'
+
+class ThemedDecoration extends React.Component {
+  
+  render () {
+    return(
+      <div>
+        <div className="children">
+          {this.props.children}
+        </div>
+      </div>
+      )
+    }
+}
+
+export default ThemedDecoration
