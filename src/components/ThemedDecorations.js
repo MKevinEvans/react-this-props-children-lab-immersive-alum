48eec1bb8e53594cf27597dea94e2215db9c085b
Kevin Evans <mkevinevans@gmail.com>
2019-08-02 19:33:30 +0000

    Automatically backed up by Learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -2,8 +2,9 @@
 import React from 'react'
 
 class ThemedDecoration extends React.Component {
-  
-  const children = this.props.children.map(child => "<div className="children"> {child} </div>")
+ 
+ const children = <div className="children"> {child} </div>
+  // const children = this.props.children.map(child => "<div className="children"> {child} </div>")
  
   render () {
     return(
